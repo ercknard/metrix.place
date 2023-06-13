5c2ebe506249a6937a3be56a2411b68b28dd51ed
SeqSEE <SeqSEE@users.noreply.github.com>
2023-06-13 16:39:38 -0400

    fix path bugs

--- src/pages/api/contract/call/index.ts
+++ src/pages/api/contract/call/index.ts
@@ -1,7 +1,8 @@
 import {RPCProvider, MetrixRPC, NetworkType} from '@metrixcoin/metrilib';
-import {RpcCredentials} from '../config/RpcCredentials';
-import {bnToHex} from '../utils/Parsers';
+
 import {NextApiRequest, NextApiResponse} from 'next';
+import {bnToHex} from '../../../../utils/Parsers';
+import {RpcCredentials} from '../../../../config/RpcCredentials';
 
 const getValues = (obj: any /* eslint-disable-line */) => {
   let values: any;

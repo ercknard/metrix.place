1366313ad90baa2bedacf7c896625160ba51395c
SeqSEE <SeqSEE@users.noreply.github.com>
2023-07-04 20:24:54 -0400

    update for prod build

--- server/db/config/config.ts
+++ server/db/config/config.ts
@@ -7,19 +7,31 @@ const config = {
     storage: path.resolve('./data', 'database.sqlite'),
     dialect: 'sqlite'
   },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
-  },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
-  }
+  test:
+    process.env.DB_DIALECT === 'sqlite'
+      ? {
+          storage: path.resolve('./data', 'database.sqlite'),
+          dialect: 'sqlite'
+        }
+      : {
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+          host: process.env.DB_HOST,
+          dialect: process.env.DB_DIALECT
+        },
+  production:
+    process.env.DB_DIALECT === 'sqlite'
+      ? {
+          storage: path.resolve('./data', 'database.sqlite'),
+          dialect: 'sqlite'
+        }
+      : {
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+          host: process.env.DB_HOST,
+          dialect: process.env.DB_DIALECT
+        }
 };
 export { config };
